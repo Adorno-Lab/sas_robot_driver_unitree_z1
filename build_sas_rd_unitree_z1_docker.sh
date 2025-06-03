2c72479aaea78eaa3744426e04353f79359d39c1
Juancho <juanjqogm@gmail.com>
2025-06-03 14:32:34 +0100

    Added support to custom the ROS_DOMAIN_ID in the docker file.

--- build_sas_rd_unitree_z1_docker.sh
+++ build_sas_rd_unitree_z1_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t sas_robot_driver_unitree_z1 Docker/sas_robot_driver_unitree_z1/
+docker build -t sas_robot_driver_unitree_z1 Docker/sas_robot_driver_unitree_z1/ --build-arg ROS_DOMAIN_ID=5
 
