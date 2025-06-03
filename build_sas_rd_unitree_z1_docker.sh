d0b59798d9ceb398629ca37b0858692b366d7133
Juancho <juanjqogm@gmail.com>
2025-06-03 14:56:26 +0100

    [build_sas_rd_unitree_z1_docker.sh] Added support for args

--- build_sas_rd_unitree_z1_docker.sh
+++ build_sas_rd_unitree_z1_docker.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-
-docker build -t sas_robot_driver_unitree_z1 Docker/sas_robot_driver_unitree_z1/ --build-arg ROS_DOMAIN_ID=5
+echo $1
+# Argument validation check
+if [ $# -eq 0 ]; then
+    echo "Error: No arguments provided."
+    echo "Usage: sh build_sas_rd_unitree_z1_docker.sh ROS_DOMAIN_ID"
+    echo "Example: sh build_sas_rd_unitree_z1_docker.sh 1"
+    exit 1
+fi
+docker build -t sas_robot_driver_unitree_z1 Docker/sas_robot_driver_unitree_z1/ --build-arg ROS_DOMAIN_ID=$1
 
