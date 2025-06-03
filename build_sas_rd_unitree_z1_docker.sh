fb1d79220927210759aad14dba25764281cfe8a3
Juancho <juanjqogm@gmail.com>
2025-06-03 11:10:11 +0100

    Added a new sh file to build the docker image

---
+++ build_sas_rd_unitree_z1_docker.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker build -t sas_robot_driver_unitree_z1 Docker/sas_robot_driver_unitree_z1/
+
---
+++ start_sas_rd_unitree_z1_docker.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run -it --name="sas_robot_driver_unitree_z1" --rm --privileged --network=host sas_robot_driver_unitree_z1 /bin/bash
