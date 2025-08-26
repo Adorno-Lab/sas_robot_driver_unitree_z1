9db55067b416e37a1fe30dc7818b056742ae032b
Juancho <juanjqogm@gmail.com>
2025-08-26 16:11:45 +0100

    updated the compose file.

--- dependencies/set_z1_path.sh
+++ dependencies/set_z1_path.sh
@@ -4,4 +4,3 @@ echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/ros2_ws/src/sas_robot_driver_uni
 echo "# Update the environment variable LIBRARY_PATH as instructed in https://ros2-tutorial.readthedocs.io" >> /etc/bash_env
 echo "export LIBRARY_PATH=$LIBRARY_PATH:~/ros2_ws/src/sas_robot_driver_unitree_z1/src/z1_sdk/lib/" >> /etc/bash_env
 
-echo "alias launch_ROS2_Z1_drivers='cdros2 && source install/setup.bash && ros2 launch sas_robot_driver_unitree_z1 real_z1_robot_launch.py ' " >> /etc/bash_env
